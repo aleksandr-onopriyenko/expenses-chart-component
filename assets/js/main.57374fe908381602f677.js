6075624863eb31d2a586ba104dfc5ea5ff3cd5d0
Aleksander <onopriyenkoalex@gmail.com>
2022-05-28 07:01:44 +0300

    fix4

--- assets/js/main.57374fe908381602f677.js
+++ assets/js/main.57374fe908381602f677.js
@@ -8,7 +8,7 @@ class t {
       this.container.insertAdjacentHTML("beforeend", `
         <div style="height: ${t}%;" class="chart-spending__item">
           <div class="tooltip">$${t}</div>
-          <div style="background: ${this.isMaxSpending ? "#76B5BCFF" : 'transparent'}"></div>
+          <div style="background: ${this.isMaxSpending ? "#76B5BCFF" : ''}"></div>
           <p>${e}</p>
         </div>
     `)
